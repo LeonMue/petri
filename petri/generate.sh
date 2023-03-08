477a18ca700973b3852855554d2b16dcc757c184
Leon Müller <leon.m111@web.de>
2023-03-08 22:05:23 +0100

    first step to make a AST => use the preprocessor jjtree

--- petri/generate.sh
+++ petri/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 COMMON_PREFIX_DIR=./src/main/java/org/dhbw/ka/ml
-javacc -OUTPUT_DIRECTORY="$COMMON_PREFIX_DIR/generated" $COMMON_PREFIX_DIR/Petri.jj
+jjtree -OUTPUT_DIRECTORY="$COMMON_PREFIX_DIR/generated" $COMMON_PREFIX_DIR/Petri.jjt
+javacc -OUTPUT_DIRECTORY="$COMMON_PREFIX_DIR/generated" $COMMON_PREFIX_DIR/generated/Petri.jj
